cefc494cbcb5cb814e327bb0b87551290a190084
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-12-08 10:57:44 -0300

    Initial setup for dependencies and folder structure

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <body>
+      <main>
+      <h1>Title</h1>
+      <h2>Subtitle</h2>
+      <p>Hello World! App.jsx</p>
+      </main>   
+    </body>
+  )
+}
+
+export default App;
---
+++ FrontEnd/src/components/global/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./assets/aionix-solutions-high-resolution-logo-grayscale-transparent-512-black.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="company-logo" />
+    </header>
+  );
+};
+
+export default Header;
